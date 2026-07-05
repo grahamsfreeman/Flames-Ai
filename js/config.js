88521864d56bb593fd2a59028433cc402378d7b1
grahamsfreeman <grahamsfreeman@gmail.com>
2026-07-05 13:46:27 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+
+    APP_NAME: "Flames Report AI",
+
+    VERSION: "1.0.0",
+
+    DEFAULT_FONT: "Times New Roman",
+
+    DEFAULT_FONT_SIZE: 12,
+
+    DEFAULT_MARGIN: "1in",
+
+    DEFAULT_CITATION: "APA",
+
+    AI_PROVIDER: "OpenAI"
+
+};
